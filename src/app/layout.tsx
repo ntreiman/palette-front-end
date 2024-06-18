d1fa564f0e100d94832c5588901d5c7e288fe111
KerriganAC <77855736+KerriganAC@users.noreply.github.com>
2024-06-17 22:15:46 -0700

    layout.tsx , added line 3 and the lines 20-22 it allows

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <HTMLHeadElement><meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /></HTMLHeadElement>
+      <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
