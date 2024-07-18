ff3a678fb24d8be45d5322670a7d5e472d80430b
bweissmann <ben.weissmann15@gmail.com>
2024-07-17 20:48:19 -0700

    add amount, change color picker style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 import { genFullGCode } from "./generator/gen-gcode";
 import { SVGRouter, getPathData } from "./svgs";
 import { hexToHSL, hslToHex } from "./utils";
+import { guessColorName } from "./color-guesser";
 
 export interface ColorEntry {
   color: HSLColor;
   svgIdentifier: string;
   name: string | undefined;
+  amount: 0 | 1 | 2;
 }
 
 export interface HSLColor {
@@ -18,72 +20,73 @@ export interface HSLColor {
 
 export default function Home() {
   const [colors, setColors] = useState<ColorEntry[]>([
-    {
-      color: {
-        h: 50,
-        s: 80,
-        l: 40,
-      },
-      name: undefined,
-      svgIdentifier: "diamond",
-    },
-    {
-      color: {
-        h: 90,
-        s: 70,
-        l: 40,
-      },
-      name: undefined,
-      svgIdentifier: "heart",
-    },
+    // {
+    //   color: {
+    //     h: 50,
+    //     s: 80,
+    //     l: 40,
+    //   },
+    //   name: undefined,
+    //   svgIdentifier: "diamond",
+    // },
+    // {
+    //   color: {
+    //     h: 90,
+    //     s: 70,
+    //     l: 40,
+    //   },
+    //   name: undefined,
+    //   svgIdentifier: "heart",
+    // },
   ]);
 
   fetch("/drawing4.svg")
     .then((response) => response.text())
     .then((d) => {
-      console.log(genFullGCode([
-        {
-          raw_svg_contents: d,
-          length_percentage: 0.20,
-          color_percentages: [0.56, 0.44, 0, 0, 0] //purple
-        },
-        {
-          raw_svg_contents: d,
-          length_percentage: 0.20,
-          color_percentages: [0.7, 0, 0, 0.3, 0] //red
-        },
-        {
-          raw_svg_contents: d,
-          length_percentage: 0.20,
-          color_percentages: [0.7, 0.07, 0.23, 0, 0] //pink
-        },
-        {
-          raw_svg_contents: d,
-          length_percentage: 0.20,
-          color_percentages: [0.0, 0.5, 0, 0.5, 0] //green
-        },
-        {
-          raw_svg_contents: d,
-          length_percentage: 0.20,
-          color_percentages: [0.375, 0, 0, 0.625, 0] //orange
-        },
-        {
-          raw_svg_contents: d,
-          length_percentage: 0.20,
-          color_percentages: [0.12, 0.88, 0, 0, 0] //dark blue
-        },
-        {
-          raw_svg_contents: d,
-          length_percentage: 0.20,
-          color_percentages: [0.096, 0.704, 0.2, 0, 0] //light blue
-        },
-        {
-          raw_svg_contents: d,
-          length_percentage: 0.20,
-          color_percentages: [0.08, 0.0, 0.42, 0.5, 0] //tan
-        }
-      ]));
-      
+      console.log(
+        genFullGCode([
+          {
+            raw_svg_contents: d,
+            length_percentage: 0.2,
+            color_percentages: [0.56, 0.44, 0, 0, 0], //purple
+          },
+          {
+            raw_svg_contents: d,
+            length_percentage: 0.2,
+            color_percentages: [0.7, 0, 0, 0.3, 0], //red
+          },
+          {
+            raw_svg_contents: d,
+            length_percentage: 0.2,
+            color_percentages: [0.7, 0.07, 0.23, 0, 0], //pink
+          },
+          {
+            raw_svg_contents: d,
+            length_percentage: 0.2,
+            color_percentages: [0.0, 0.5, 0, 0.5, 0], //green
+          },
+          {
+            raw_svg_contents: d,
+            length_percentage: 0.2,
+            color_percentages: [0.375, 0, 0, 0.625, 0], //orange
+          },
+          {
+            raw_svg_contents: d,
+            length_percentage: 0.2,
+            color_percentages: [0.12, 0.88, 0, 0, 0], //dark blue
+          },
+          {
+            raw_svg_contents: d,
+            length_percentage: 0.2,
+            color_percentages: [0.096, 0.704, 0.2, 0, 0], //light blue
+          },
+          {
+            raw_svg_contents: d,
+            length_percentage: 0.2,
+            color_percentages: [0.08, 0.0, 0.42, 0.5, 0], //tan
+          },
+        ])
+      );
     });
 
   const handleColorPickerChange = (index: number, value: string) => {
@@ -91,7 +94,16 @@ export default function Home() {
     setColors((colors) =>
       colors.map((c, i) => {
         if (i === index) {
-          return { ...c, color: hslColor };
+          const updatedName =
+            c.name !== guessColorName(hslToHex(c.color))
+              ? c.name
+              : guessColorName(hslToHex(hslColor));
+          return {
+            name: updatedName,
+            svgIdentifier: c.svgIdentifier,
+            color: hslColor,
+            amount: c.amount,
+          };
         }
         return c;
       })
@@ -109,55 +121,76 @@ export default function Home() {
     );
   };
 
+  const handleAmountChange = (index: number, amount: 0 | 1 | 2) => {
+    setColors((colors) =>
+      colors.map((c, i) => {
+        if (i === index) {
+          return { ...c, amount };
+        }
+        return c;
+      })
+    );
+  };
+
   const addColor = () => {
     let c: HSLColor = { h: 0, s: 0, l: 0 };
     setColors((colors) => [
       ...colors,
-      { color: c, name: undefined, svgIdentifier: "diamond" },
+      {
+        color: c,
+        amount: 1,
+        name: guessColorName(hslToHex(c)),
+        svgIdentifier: "diamond",
+      },
     ]);
   };
 
   const sendToPalette = async () => {
-    const endpoint = 'https://obsidiancafe.com/files'; // Endpoint updated to include /files
+    const endpoint = "https://obsidiancafe.com/files"; // Endpoint updated to include /files
     const formData = new FormData();
 
     // Append colors data (JSON) to FormData
-    formData.append('colors', JSON.stringify(colors));
+    formData.append("colors", JSON.stringify(colors));
 
     // Create text content from colors.map data (including SVG path data)
-    const textContent = colors.map((c) => (
-      `Color: ${c.color}, SVG Identifier: ${c.svgIdentifier}, Name: ${c.name}, Path Data: ${getPathData(c.svgIdentifier)}`
-    )).join('\n');
+    const textContent = colors
+      .map(
+        (c) =>
+          `Color: ${c.color}, SVG Identifier: ${c.svgIdentifier}, Name: ${
+            c.name
+          }, Path Data: ${getPathData(c.svgIdentifier)}`
+      )
+      .join("\n");
 
     // Create a blob from the text content
-    const blob = new Blob([textContent], { type: 'text/plain' });
+    const blob = new Blob([textContent], { type: "text/plain" });
 
     // Append additional parameters
-    formData.append('path', '/');
-    formData.append('/colors.txtS', '1');
+    formData.append("path", "/");
+    formData.append("/colors.txtS", "1");
 
     // Append the blob to FormData with a static file name and extension
-    formData.append('myfile[]', blob, '/colors.txt');
+    formData.append("myfile[]", blob, "/colors.txt");
 
     // Send the request
     try {
       const response = await fetch(endpoint, {
-        method: 'POST',
+        method: "POST",
         body: formData,
         headers: {
-          'Accept': 'application/json',
+          Accept: "application/json",
         },
       });
 
       if (response.ok) {
-        console.log('File sent successfully');
+        console.log("File sent successfully");
         // Handle successful file transmission here
       } else {
-        console.error('Failed to send file:', await response.text());
+        console.error("Failed to send file:", await response.text());
         // Handle server errors here
       }
     } catch (error) {
-      console.error('Network error:', error);
+      console.error("Network error:", error);
       // Handle network errors here
     }
   };
@@ -167,63 +200,107 @@ export default function Home() {
   };
 
   return (
-    <div className="h-screen bg-[#e4b3c880] p-4 overflow-x-clip">
+    <div className="h-screen bg-[#676767] p-4 overflow-x-clip">
       <div className="flex flex-col md:flex-row h-full md:items-start">
         <div className="bg-blue-50 p-2 rounded">
           <h1 className="text-2xl">
-            Customize your{" "}
-            <span className="text-[#e4b3c8]">
-              <i>Palette</i>
+            Pick your{" "}
+            <span className="text-[#ffb5e1]">
+              <b>
+                <i>Colors</i>
+              </b>
             </span>
           </h1>
           <hr className="mb-2 mt-1 border-black/20" />
           <div className="flex flex-col items-center">
-            <div className="flex flex-col items-stretch min-w-[300px]">
+            <div className="flex flex-col space-y-4 items-stretch min-w-[300px]">
               {colors.map((c, i) => (
                 <div className="flex justify-between items-center" key={i}>
                   <div className="flex space-x-2">
                     <button
                       onClick={() => removeColor(i)}
-                      className="text-red-500 w-4"
+                      className="text-red-500 w-4 text-xl mr-1"
                     >
                       ⊖
                     </button>
-                    <input
-                      className="w-[80px]"
-                      value={c.name ?? `Color ${i + 1}`}
-                      onChange={(e) => handleRename(i, e.target.value)}
-                    ></input>
+                    <div className="w-[200px]">
+                      <input
+                        className="w-full h-7 mb-1 pl-1"
+                        value={c.name ?? `Color ${i + 1}`}
+                        onChange={(e) => handleRename(i, e.target.value)}
+                      ></input>
+                      <div className="h-6 text-xs flex">
+                        {[0, 1, 2].map((amount) => (
+                          <button
+                            key={amount}
+                            onClick={() =>
+                              handleAmountChange(i, amount as 0 | 1 | 2)
+                            }
+                            className={`h-full w-full border rounded text-center ${
+                              amount == c.amount
+                                ? "bg-blue-200"
+                                : "bg-blue-200/10"
+                            }`}
+                          >
+                            {Array(amount + 1)
+                              .fill("o")
+                              .join("")}
+                          </button>
+                        ))}
+                      </div>
+                    </div>
                   </div>
-                  <div className="flex space-x-4">
+                  <div className="flex space-x-4 h-12 w-[100px] relative">
                     <input
                       type="color"
+                      className="h-full w-full"
+                      style={{
+                        borderRadius: "14px",
+                        overflow: "hidden",
+                        border: "",
+                      }}
                       value={hslToHex(c.color)}
-                      onChange={(e) => handleColorPickerChange(i, e.target.value)}
+                      onChange={(e) =>
+                        handleColorPickerChange(i, e.target.value)
+                      }
                     />
-                    <div className="w-[25px] h-[25px]">
-                      {c.svgIdentifier.length > 0 && (
-                        <SVGRouter
-                          svgIdentifier={c.svgIdentifier}
-                          color={hslToHex(c.color)}
-                        />
-                      )}
-                    </div>
+                    {c.color.l < 5 && (
+                      <div className="absolute pointer-events-none top-0 -left-4 w-full h-full flex items-center justify-center">
+                        <div className="m-0 p-0 text-xs text-white text-center">
+                          Tap to change color
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </div>
               ))}
-              <button
-                className="border border-black hover:bg-gray-100 px-2 py-1 font-semibold rounded mx-2 my-2"
-                onClick={addColor}
-              >
-                ➕ Add Color
-              </button>
-              <button
-                className="flex items-center justify-center border bg-black hover:bg-gray-800 border-white text-white px-2 py-1 font-semibold rounded mx-2 mb-2"
-                onClick={sendToPalette}
-              >
-                <img className="h-[20px] mr-2 mt-1" src="plogo.png" alt="logo" />
-                Go
-              </button>
+
+              <div className="flex flex-col">
+                <button
+                  className="border border-black hover:bg-gray-100 px-2 py-1 font-semibold rounded mx-2 my-2"
+                  onClick={addColor}
+                >
+                  ➕ Add Color
+                </button>
+                {colors.length > 0 && (
+                  <button
+                    className="flex items-center justify-center border bg-black hover:bg-gray-800 border-white text-white px-2 py-1 font-semibold rounded mx-2 mb-2"
+                    onClick={sendToPalette}
+                  >
+                    <img
+                      className="h-[20px] mr-2 mt-1"
+                      src="plogo.png"
+                      alt="logo"
+                    />
+                    Go
+                  </button>
+                )}
+                <p className="text-gray-500 mx-2 text-center">
+                  {colors.length == 0
+                    ? "Add a color to start building your color palette"
+                    : "Add more colors to round out your palette"}
+                </p>
+              </div>
             </div>
           </div>
         </div>
@@ -253,7 +330,12 @@ function Circle({ entries }: { entries: ColorEntry[] }) {
         >
           <div
             className="absolute top-8 w-16 h-16"
-            style={{ left: "calc(50% - 32px)" }}
+            style={{ 
+              left: "calc(50% - 32px)", 
+              scale: (e.amount == 0) ? 0.75 : (
+                (e.amount == 1) ? 1 : 1.25
+              )
+            }}
           >
             {e.svgIdentifier.length > 0 && (
               <SVGRouter
